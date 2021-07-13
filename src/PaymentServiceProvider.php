71d7705c21ac88d8af32d14a1f4f1c9696229365
Sohrab Gosheh <sohrab.gh727272@gmail.com>
2021-07-13 13:50:24 +0430

    first initial

---
+++ src/PaymentServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace hooshmand\payment;
+
+use Illuminate\Support\ServiceProvider;
+
+class PaymentServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
